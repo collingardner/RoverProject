571e09539448ff32e932b6b6099ad52b85954d65
Collin Gardner <gardnecr@miamioh.edu>
2017-03-14 15:02:27 -0400

    Bluetooth Controller has been Debugged
    The bluetooth wasnt working correctly, had some pins not programmed
    correctly in a file

---
+++ controlMod/controlMod.ino
@@ -0,0 +1,257 @@
+
+#include <string.h>
+#include <Arduino.h>
+#include <SPI.h>
+#if not defined (_VARIANT_ARDUINO_DUE_X_) && not defined (_VARIANT_ARDUINO_ZERO_)
+  #include <SoftwareSerial.h>
+#endif
+
+#include "Adafruit_BLE.h"
+#include "Adafruit_BluefruitLE_SPI.h"
+#include "Adafruit_BluefruitLE_UART.h"
+
+#include "BluefruitConfig.h"
+
+//#define BLUEFRUIT_SWUART_RXD_PIN 10
+//#define BLUEFRUIT_SWUART_TXD_PIN 11
+//#define BLUEFRUIT_UART_CTS_PIN 12
+//#define BLUEFRUIT_UART_RTS_PIN 4
+//
+//#define BLUEFRUIT_UART_MODE_PIN 13
+
+    #define FACTORYRESET_ENABLE         1
+    #define MINIMUM_FIRMWARE_VERSION    "0.6.6"
+    #define MODE_LED_BEHAVIOUR          "MODE"
+/*=========================================================================*/
+/*                SETTING THE PINS FOR SENSOR AND ROVOR SHIELD    */
+//Pins from the Ultrasonic Sensor
+int triggerPin = 2; //triggering on pin 2
+int echoPin = 3;    //echo on pin 3
+
+//Pins for the shield
+int lightPin = A0;
+int thermoPin = A1;
+int rightMotorPin = 5;
+int leftMotorPin = 6;
+int rightDirectionPin = 7;
+int leftDirectionPin = 8;
+int leftspeed = 255; //255 is maximum speed
+int rightspeed = 255;
+
+
+// Create the bluefruit object, either software serial...uncomment these lines
+
+SoftwareSerial bluefruitSS = SoftwareSerial(BLUEFRUIT_SWUART_TXD_PIN, BLUEFRUIT_SWUART_RXD_PIN);
+
+Adafruit_BluefruitLE_UART ble(bluefruitSS, BLUEFRUIT_UART_MODE_PIN,
+                      BLUEFRUIT_UART_CTS_PIN, BLUEFRUIT_UART_RTS_PIN);
+
+
+/* ...or hardware serial, which does not need the RTS/CTS pins. Uncomment this line */
+// Adafruit_BluefruitLE_UART ble(BLUEFRUIT_HWSERIAL_NAME, BLUEFRUIT_UART_MODE_PIN);
+
+/* ...hardware SPI, using SCK/MOSI/MISO hardware SPI pins and then user selected CS/IRQ/RST */
+//Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
+
+/* ...software SPI, using SCK/MOSI/MISO user-defined SPI pins and then user selected CS/IRQ/RST */
+//Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_SCK, BLUEFRUIT_SPI_MISO,
+//                             BLUEFRUIT_SPI_MOSI, BLUEFRUIT_SPI_CS,
+//                             BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
+
+
+// A small helper
+void error(const __FlashStringHelper*err) {
+  Serial.println(err);
+  while (1);
+}
+
+// function prototypes over in packetparser.cpp
+uint8_t readPacket(Adafruit_BLE *ble, uint16_t timeout);
+float parsefloat(uint8_t *buffer);
+void printHex(const uint8_t * data, const uint32_t numBytes);
+
+// the packet buffer
+extern uint8_t packetbuffer[];
+
+/*=====================*/
+void setup(void)
+{
+  
+  /* SETUP for SENSOR and ROVER SHEILD */
+  pinMode(triggerPin, OUTPUT); //defining pins
+  pinMode(echoPin, INPUT);
+
+  //Shield Code
+  pinMode(lightPin, INPUT);
+  pinMode(thermoPin, INPUT);
+  pinMode(rightMotorPin, OUTPUT);
+  pinMode(leftMotorPin, OUTPUT);
+  pinMode(rightDirectionPin, OUTPUT);
+  pinMode(leftDirectionPin, OUTPUT);
+
+
+  while (!Serial);  // required for Flora & Micro
+  delay(500);
+
+  Serial.begin(115200);
+  Serial.println(F("Adafruit Bluefruit App Controller Example"));
+  Serial.println(F("-----------------------------------------"));
+
+  /* Initialise the module */
+  Serial.print(F("Initialising the Bluefruit LE module: "));
+
+  if ( !ble.begin(VERBOSE_MODE) )
+  {
+    error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
+  }
+  Serial.println( F("OK!") );
+
+
+  /* Disable command echo from Bluefruit */
+  ble.echo(false);
+
+  Serial.println("Requesting Bluefruit info:");
+  /* Print Bluefruit information */
+  ble.info();
+
+  Serial.println(F("Please use Adafruit Bluefruit LE app to connect in Controller mode"));
+  Serial.println(F("Then activate/use the sensors, color picker, game controller, etc!"));
+  Serial.println();
+
+  ble.verbose(false);  // debug info is a little annoying after this point!
+
+  /* Wait for connection */
+  while (! ble.isConnected()) {
+      delay(500);
+  }
+
+  Serial.println(F("******************************"));
+
+  // LED Activity command is only supported from 0.6.6
+  if ( ble.isVersionAtLeast(MINIMUM_FIRMWARE_VERSION) )
+  {
+    // Change Mode LED Activity
+    Serial.println(F("Change LED activity to " MODE_LED_BEHAVIOUR));
+    ble.sendCommandCheckOK("AT+HWModeLED=" MODE_LED_BEHAVIOUR);
+  }
+
+  // Set Bluefruit to DATA mode
+  Serial.println( F("Switching to DATA mode!") );
+  ble.setMode(BLUEFRUIT_MODE_DATA);
+
+  Serial.println(F("******************************"));
+
+}
+
+/**************************************************************************/
+/*!
+    @brief  Constantly poll for new command or response data
+*/
+/**************************************************************************/
+void loop(void)
+{
+  /* Wait for new data to arrive */
+  uint8_t len = readPacket(&ble, BLE_READPACKET_TIMEOUT);
+  if (len == 0) return;
+
+  /* Got a packet! */
+  // printHex(packetbuffer, len);
+
+//  // Color
+//  if (packetbuffer[1] == 'C') {
+//    uint8_t red = packetbuffer[2];
+//    uint8_t green = packetbuffer[3];
+//    uint8_t blue = packetbuffer[4];
+//    Serial.print ("RGB #");
+//    if (red < 0x10) Serial.print("0");
+//    Serial.print(red, HEX);
+//    if (green < 0x10) Serial.print("0");
+//    Serial.print(green, HEX);
+//    if (blue < 0x10) Serial.print("0");
+//    Serial.println(blue, HEX);
+//  }
+  int duration, distance;    //Adding duration and distance
+  
+  digitalWrite(triggerPin, HIGH); //triggering the wave(like blinking an LED)
+  delay(10);
+  digitalWrite(triggerPin, LOW);
+  
+  duration = pulseIn(echoPin, HIGH); //a special function for listening and waiting for the wave
+  distance = (duration/2) / 29.1; //transforming the number to cm(if you want inches, you have to change the 29.1 with a suitable number
+  
+  Serial.print(distance);    //printing the numbers
+  Serial.print("cm");       //and the unit
+  Serial.println(" ");      //just printing to a new line
+  
+  //IF YOU WANT THE PORGRAM SPITTING OUT INFORMATION SLOWER, JUST UNCOMMENT(DELETE THE 2 //) THE NEXT LINE AND CHANGE THE NUMBER
+  //delay(500);
+
+
+
+
+
+
+
+  // Buttons
+  if (packetbuffer[1] == 'B') {
+    uint8_t buttnum = packetbuffer[2] - '0';
+    boolean pressed = packetbuffer[3] - '0';
+   // Serial.print ("Button "); Serial.print(buttnum);
+    if (buttnum == 5 && pressed == true) {
+     forward (leftspeed, rightspeed);
+    }
+   else if (buttnum == 7 && pressed == true) {
+     left (leftspeed, rightspeed);
+    }
+   else if (buttnum == 8 && pressed == true) {
+     right (leftspeed, rightspeed);
+    }
+   else if (buttnum == 6 && pressed == true) {
+     reverse (leftspeed, rightspeed);
+    }
+    else {
+      stop();
+    }
+  }
+}
+//METHODS FOR TRAVELING
+void stop(void) //Stop
+{
+  digitalWrite(leftMotorPin, LOW);
+  digitalWrite(rightMotorPin, LOW);
+}
+
+void forward(char a, char b)
+{
+  analogWrite (leftMotorPin, a);
+  digitalWrite(leftDirectionPin, LOW);
+  analogWrite (rightMotorPin, b);
+  digitalWrite(rightDirectionPin, LOW);
+}
+
+void reverse (char a, char b)
+{
+  analogWrite (leftMotorPin, a);
+  digitalWrite(leftDirectionPin, HIGH);
+  analogWrite (rightMotorPin, b);
+  digitalWrite(rightDirectionPin, HIGH);
+}
+
+void left (char a, char b)
+{
+  analogWrite (leftMotorPin, a);
+  digitalWrite(leftDirectionPin, HIGH);
+  analogWrite (rightMotorPin, b);
+  digitalWrite(rightDirectionPin, LOW);
+}
+
+void right (char a, char b)
+{
+  analogWrite (leftMotorPin, a);
+  digitalWrite(leftDirectionPin, LOW);
+  analogWrite (rightMotorPin, b);
+  digitalWrite(rightDirectionPin, HIGH);
+}
+
+
+
